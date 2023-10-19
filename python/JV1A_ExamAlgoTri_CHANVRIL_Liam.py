e20535a6c6ae79eec223c89423a40a92ca1e9aa9
Jusdebanane <118273696+Jusdebanane@users.noreply.github.com>
2023-10-19 17:37:33 +0200

    corrigé

--- python/JV1A_ExamAlgoTri_CHANVRIL_Liam.py
+++ python/JV1A_ExamAlgoTri_CHANVRIL_Liam.py
@@ -1,13 +1,13 @@
 #1
-
-save = 0
 myTable =[2,5,1,3,4]
 
 print(myTable)
+def switch(tableau,index1,index2):
+    save = tableau[index1]
+    tableau[index1] = tableau[index2]
+    tableau[index2] = save
 
-save = myTable[2]
-myTable[2] = myTable[1]
-myTable[1] = save
+switch(myTable,1,2)
 
 print(myTable)
 print("\n")
@@ -19,9 +19,7 @@ print(myTable)
 
 for i in range(len(myTable)-1):
     if(myTable[i]>myTable[i+1]):
-        save = myTable[i]
-        myTable[i] = myTable[i+1]
-        myTable[i+1] = save
+        switch(myTable,i,(i+1))
 
 print(myTable)
 print("\n")
@@ -35,15 +33,39 @@ print(myTable)
 for i in range(len(myTable)-1):
     for j in range(len(myTable)-1):
         if(myTable[j]>myTable[j+1]):
-            save = myTable[j]
-            myTable[j] = myTable[j+1]
-            myTable[j+1] = save
+            switch(myTable,j,(j+1))
+
+print(myTable)
+print("\n")
+
+#upgrade
+#1
+myTable = [2,0,4,1,5,3]
 
 print(myTable)
 
-#4
+for i in range(len(myTable)-1):
+    for j in range(len(myTable)-1-i):
+        if(myTable[j]>myTable[j+1]):
+            switch(myTable,j,(j+1))
 
-#Le tri est bulle est lent puisqu'il doit parcourir plusieur fois le tableau afin d'en comparer tout ses membres et les déplacer a chaque fois.
-# Pour un petit tableau comme ici cela n'est pas dérangeant mais pour des tableau plus conséquent, cela multiplie le processus encore et encore 
-# ce qui peut utiliser beaucoup de performances. Il doit etre possible de calculer le temps de chaque processus en prenant en compte la vitesse
-# calcul du processeur ainsi que le poid en octet du processus.
+print(myTable)
+print("\n")
+
+#2
+
+nonTrie = True
+myTable = [2,0,4,1,5,3]
+
+print(myTable)
+
+while (nonTrie):
+    nonTrie = False
+
+    for j in range(len(myTable)-1-i):
+        if(myTable[j]>myTable[j+1]):
+            switch(myTable,j,(j+1))
+            nonTrie = True
+
+print(myTable)
+print("\n")
