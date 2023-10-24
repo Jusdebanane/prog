09bc3bf09e5775ca422567374748545cdeba73dc
Jusdebanane <118273696+Jusdebanane@users.noreply.github.com>
2023-10-24 09:23:18 +0200

    Update quick_sort.py

--- python/quick_sort.py
+++ python/quick_sort.py
@@ -1,41 +1,13 @@
-#ne fonctionne pas !!!
+#a ameliorer !
 
 import random
-liste = []
-
+liste = [10,80,30,90,40]
+trie = False
 
 def swap(tableau,index1,index2):
     save = tableau[index1]
     tableau[index1] = tableau[index2]
     tableau[index2] = save
 
-def part(tableau,first,last,pivot):
-    swap(tableau,pivot,last)
-
-    j = first
-
-    for i in range(first,(last-1)):
-        if(tableau[i] <= tableau[last]):
-            swap(tableau,i,j)
-            j += 1
-    
-    swap(tableau,last,j)
-    return j
-
-def quick_sort(tableau,first,last):
-    if(first < last):
-
-        pivot = first
-        part(tableau,first,last,pivot)
-
-        quick_sort(tableau,first,(pivot-1))
-        quick_sort(tableau,(pivot+1),last)
-
-for i in range (16):
-    liste.append(random.randint(0,100))
-
-print(liste)
-
-quick_sort(liste,0,15)
-
-print(liste)
+def partition(pivot,left,right):
+    left = liste[len(liste-1)]
